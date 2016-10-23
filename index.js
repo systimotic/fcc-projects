1b90a5d4a29c641f7aa29ce40a26a5772e2b962b
systimotic <timogos.k@live.nl>
2016-10-23 17:06:58 +0200

    general/shorten: get set for shorten

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 5000;
 
 const timestamp = require('./timestamp');
 const whoami = require('./whoami');
+const shorten = require('./shorten');
 
 app.use('/timestamp', timestamp);
 app.use('/whoami', whoami);
+app.use('/shorten', shorten);
 
 app.listen(port, () => {
   console.log('Listening on port', port);
