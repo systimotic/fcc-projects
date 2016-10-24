a6f0b65a391131fd4c33ce3c6f5738c180287e26
systimotic <timogos.k@live.nl>
2016-10-24 22:46:23 +0200

    image-search: complete project

--- image-search/index.js
+++ image-search/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoosePackage = require('mongoose');
+const mongoose = new mongoosePackage.Mongoose;
+const request = require('request');
 const app = express.Router();
-const ShortLink = require('./querySchema');
+const Query = require('./querySchema');
 
-const dbPath = process.env.DB || 'localhost:27017/image-search';
+const dbPath = process.env.DB_IMAGE_SEARCH || 'localhost:27017/image-search';
+const key = process.env.BING_SEARCH_KEY;
+
+function composeURL(query, offset) {
+  query = encodeURIComponent(query);
+  return `https://api.cognitive.microsoft.com/bing/v5.0/images/search?count=10&safeSearch=Moderate&q=${query}&offset=${offset}`;
+}
 
 mongoose.connect(dbPath);
 mongoose.connection.on('error', console.error);
@@ -13,30 +21,46 @@ app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
-app.get('/:query', (req, res) => {
-  // const newLink = new ShortLink({ id, url });
-  // newLink.save((err) => {
-  //   if (err) return console.error(err);
-  //
-  //   res.json({
-  //     original_url: url,
-  //     short_url: baseURL + id
-  //   });
-  // });
+app.get('/recent', (req, res) => {
+  Query.find({}, { _id: 0, __v: 0 }).sort({ date: -1 }).limit(10).exec((err, docs) => {
+    if (err) return console.error(err);
+
+    res.json(docs);
+  });
 });
 
-app.get('/recent', (req, res) => {
-  // ShortLink.findOne({ id }, (err, link) => {
-  //   if (err) return console.error(err);
-  //
-  //   if (!link) {
-  //     return res.json({
-  //       error: 'There is no link with this ID.'
-  //     });
-  //   }
-  //
-  //   res.redirect(link.url);
-  // });
+app.get('/:query', (req, res) => {
+  const userQuery = req.params.query;
+  const parsedOffset = req.query.offset;
+  const offset = parsedOffset > 0 && parsedOffset <= 100 ? parsedOffset * 10 : 0;
+  const date = Date.now();
+
+  request({
+    url: composeURL(userQuery, offset),
+    headers: {
+      'Ocp-Apim-Subscription-Key': key
+    }
+  }, (err, response, body) => {
+    if (err || response.statusCode !== 200) return console.error(err, response.statusCode);
+
+    body = JSON.parse(body);
+
+    const output = [];
+    body.value.forEach((item) => {
+      output.push({
+        image: item.contentUrl,
+        alt: item.name,
+        source: item.hostPageUrl
+      });
+    });
+
+    res.json(output);
+  });
+
+  const newQuery = new Query({ query: userQuery, date });
+  newQuery.save((err) => {
+    if (err) return console.error(err);
+  });
 });
 
 
--- image-search/querySchema.js
+++ image-search/querySchema.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const querySchema = new mongoose.Schema({
   query: String,
-  date: {
-    type: Date,
-    default: Date.now()
-  }
+  date: Date
 });
 
 module.exports = mongoose.model('Query', querySchema);
