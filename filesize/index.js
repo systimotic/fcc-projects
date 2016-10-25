d485ff6cb78286f75baf9a59cdce978beac044b8
systimotic <timogos.k@live.nl>
2016-10-25 21:52:21 +0200

    filesize: complete project

--- filesize/index.js
+++ filesize/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express.Router();
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.sendFile(__dirname + '/filesize.html');
+});
+
+app.post('/submit', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    res.json({
+      error: 'Please submit a file'
+    });
+  }
+
+  res.json({
+    size: req.file.size
+  });
 });
 
 
